16c3ae9b000b250ebbf8999c1057ed02c9bbaf5f
DPeet <bearbiever@gmail.com>
2015-04-14 16:07:44 -0400

    package change

--- shared/collections.js
+++ shared/collections.js
@@ -11,3 +11,17 @@ Teams.allow({
     return true;
   }
 });
+
+Games =  new Meteor.Collection("games");
+
+Games.allow({
+  insert: function(){
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function(){
+    return true;
+  }
+});
